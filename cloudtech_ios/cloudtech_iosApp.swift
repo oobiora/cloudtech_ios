083930a03861296bf49e33e4d2747485c8498170
Olisa Obiora <oobiora22@gmail.com>
2021-10-16 12:23:12 -0400

    Initial Commit

---
+++ cloudtech_ios/cloudtech_iosApp.swift
@@ -0,0 +1,20 @@
+//
+//  cloudtech_iosApp.swift
+//  cloudtech_ios
+//
+//  Created by Olisa Obiora on 10/16/21.
+//
+
+import SwiftUI
+
+@main
+struct cloudtech_iosApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
